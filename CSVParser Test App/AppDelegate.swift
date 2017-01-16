785bab13ee9b49bdec6142c9c3e5d59ed33838f9
chrispysoft <chrispysoft@icloud.com>
2017-01-16 18:09:08 +0100

    Initial Commit

---
+++ CSVParser Test App/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  CSVParser Test App
+//
+//  Created by Chris on 16/01/2017.
+//  Copyright © 2017 chrispysoft. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
