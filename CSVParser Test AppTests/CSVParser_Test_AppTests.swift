6d4671660ff2995d430c076cfd22c9666b95a71a
chrispysoft <chrispysoft@icloud.com>
2017-01-17 11:57:40 +0100

    Added testTokenizerBasic()

--- CSVParser Test AppTests/CSVParser_Test_AppTests.swift
+++ CSVParser Test AppTests/CSVParser_Test_AppTests.swift
@@ -18,18 +18,52 @@ class CSVParser_Test_AppTests: XCTestCase {
 		super.tearDown()
 	}
 	
-	
-    func testTokens() {
-		let input = ["1","2","3","4","5","6"]
-		let data = input.joined().data(using: .utf8)!
-		let csvdoc = CSVDocument(data: data)
+	func testTokenizerBasic() {
+		let data = "1,2,3\n4,5,6".data(using: .utf8)!
+		let expectedTokens = [
+			CSVToken(type: .Character, content: "1"),
+			CSVToken(type: .Delimiter, content: ","),
+			CSVToken(type: .Character, content: "2"),
+			CSVToken(type: .Delimiter, content: ","),
+			CSVToken(type: .Character, content: "3"),
+			CSVToken(type: .LineSeparator, content: "\n"),
+			CSVToken(type: .Character, content: "4"),
+			CSVToken(type: .Delimiter, content: ","),
+			CSVToken(type: .Character, content: "5"),
+			CSVToken(type: .Delimiter, content: ","),
+			CSVToken(type: .Character, content: "6")
+		]
 		
-		var idx = 0
-		for line in csvdoc {
-			XCTAssertEqual(line.first!, input[idx])
-			idx += 1
+		let tokenizer = UTF8DataTokenizer(data: data)
+		
+		var tokens = [CSVToken]()
+		while let token = tokenizer.nextToken() {
+			tokens.append(token)
 		}
 		
+		XCTAssertEqual(tokens.count, expectedTokens.count, "Did not receive expected number of tokens")
+		
+		for i in tokens.indices where expectedTokens.indices.contains(i) {
+			XCTAssertEqual(tokens[i].type, expectedTokens[i].type, "Token Type at index \(i) does not match")
+			XCTAssertEqual(tokens[i].content, expectedTokens[i].content, "Token Content at index \(i) does not match")
+		}
+	}
+
+	
+	
+	func testBasicFile() {
+		let data = "1,2,3\n4,5,6".data(using: .utf8)!
+		let result = [["1","2","3"],["4","5","6"]]
+		
+		let csvdoc = CSVDocument(data: data)
+		
+		for (i, line) in csvdoc.enumerated() {
+			guard result.indices.contains(i) else {
+				XCTFail("Parser returned too many lines")
+				break
+			}
+			XCTAssertEqual(line, result[i])
+		}
 	}
 	
 }
