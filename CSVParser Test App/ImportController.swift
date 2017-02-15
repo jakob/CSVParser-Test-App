22321bb9b5adbaf209a66d078d9a3b653081749b
chrispysoft <chrispysoft@icloud.com>
2017-02-15 18:53:58 +0100

    Added basic import features

--- CSVParser Test App/ImportController.swift
+++ CSVParser Test App/ImportController.swift
@@ -8,15 +8,80 @@
 
 import Cocoa
 
-class ImportController {
+class ImportController: NSObject {
 	
-	static var activeImportControllers = [ImportController]()
+	private static var activeImportControllers = [ImportController]()
+	
+	fileprivate var tableData = [[String]]()
+	private var headerData = [String]()
+	
+	private var fileURL: URL?
+	
+	private var delimiterCharacter: Character {
+		var char: String
+		let selIdx = delimiterSegControl?.selectedSegment ?? 0
+		switch selIdx {
+			case 0: char = ","
+			case 1: char = ";"
+			case 2: char = "\t"
+			case 3: char = "|"
+			default: char = ","
+		}
+		return Character(char)
+	}
+	private var decimalCharacter: Character {
+		var char: String
+		let selIdx = decimalSegControl?.selectedSegment ?? 0
+		switch selIdx {
+			case 0: char = "."
+			case 1: char = ","
+			default: char = "."
+		}
+		return Character(char)
+	}
+	private var quoteCharacter: Character {
+		var char: String
+		let selIdx = quoteSegControl?.selectedSegment ?? 0
+		switch selIdx {
+			case 0: char = "\""
+			case 1: char = ""
+			case 2: char = ""
+			default: char = "\""
+		}
+		return Character(char)
+	}
+	private var encoding: String.Encoding {
+		guard let selectedItem = encodingPopupButton?.selectedItem else { return .utf8 }
+		switch selectedItem.title {
+			case "UTF-8": return .utf8
+			default: return .utf8
+		}
+	}
+	private var firstRowAsHeader: Bool {
+		guard let checkbox = firstRowAsHeaderCheckbox else { return false }
+		return checkbox.state == NSOnState
+	}
+	private var importNumRows = 10
 	
 	@IBOutlet var settingsWindow: NSWindow?
+	@IBOutlet var delimiterSegControl: NSSegmentedControl?
+	@IBOutlet var decimalSegControl: NSSegmentedControl?
+	@IBOutlet var quoteSegControl: NSSegmentedControl?
+	@IBOutlet var encodingPopupButton: NSPopUpButton?
+	@IBOutlet var firstRowAsHeaderCheckbox: NSButton?
+	@IBOutlet var tableView: NSTableView?
+	
+	
+	@IBAction func configChanged(_ sender: AnyObject?) {
+		importFile()
+	}
 	
 	
 	func startImport() {
 		ImportController.activeImportControllers.append(self)
+		
+		Bundle.main.loadNibNamed("ImportWindow", owner: self, topLevelObjects: nil)
+		settingsWindow?.makeKeyAndOrderFront(nil)
 		
 		let openPanel = NSOpenPanel()
 		openPanel.canChooseFiles = true
@@ -24,34 +89,79 @@ class ImportController {
 		openPanel.allowsMultipleSelection = false
 		openPanel.allowedFileTypes = ["csv"]
 		
+		if openPanel.runModal() == NSFileHandlingPanelOKButton {
+			fileURL = openPanel.urls.first!
+			importFile()
+		}
 		
-		//guard openPanel.runModal() == NSFileHandlingPanelOKButton, let fileURL = openPanel.urls.first else { return }
-		
-		
-//		if openPanel.runModal() == NSFileHandlingPanelOKButton {
-//			let fileURL = openPanel.urls.first!
-//			let csvDoc = CSVDocument(fileURL: fileURL)
-//			let iterator = csvDoc.makeIterator()
-//			while let elem = iterator.next() {
-//				while let warning = iterator.nextWarning() {
-//					print("WARNING: \(warning.text)")
-//				}
-//				XCTAssertEqual(elem.count, expected[idx].count, "Line \(idx) does not have expected length")
-//				XCTAssertEqual(elem, expected[idx], "Line \(idx) is not equal")
-//			}
-//			
-//			while let warning = iterator.nextWarning() {
-//				print("WARNING: \(warning.text)")
-//			}
-//		}
-		
-		Bundle.main.loadNibNamed("ImportWindow", owner: self, topLevelObjects: nil)
-		settingsWindow?.makeKeyAndOrderFront(nil)
-		
-		
-		
-		
-		// nremover self 
+		// remove self from activeImportControllers
 	}
 	
+	func importFile() {
+		var config = CSVConfig()
+		config.delimiterCharacter = delimiterCharacter
+		config.decimalCharacter = decimalCharacter
+		config.quoteCharacter = quoteCharacter
+		config.encoding = encoding
+		
+		guard let fileURL = fileURL else { print("fileURL is nil"); return }
+		
+		let csvDoc = CSVDocument(fileURL: fileURL, config: config)
+		let iterator = csvDoc.makeIterator()
+		var nRows = 0
+		var nColumns = 0
+		
+		tableData.removeAll()
+		headerData.removeAll()
+		
+		while let elem = iterator.next() {
+			while let warning = iterator.nextWarning() { print("WARNING: \(warning.text)") }
+			
+			if firstRowAsHeader && nRows == 0 {
+				headerData.append(contentsOf: elem)
+			} else {
+				tableData.append(elem)
+			}
+			
+			if elem.count > nColumns {
+				nColumns = elem.count
+			}
+			
+			nRows += 1
+			
+			if nRows >= importNumRows {
+				break
+			}
+		}
+		while let warning = iterator.nextWarning() { print("WARNING: \(warning.text)") }
+		
+		while let col = tableView?.tableColumns.last {
+			tableView?.removeTableColumn(col)
+		}
+		for i in 0..<nColumns {
+			let col = NSTableColumn(identifier: "\(i)")
+			if firstRowAsHeader && headerData.indices.contains(i) {
+				col.headerCell.title = headerData[i]
+			}
+			tableView?.addTableColumn(col)
+		}
+		
+		tableView?.reloadData()
+	}
 }
+
+extension ImportController: NSTableViewDataSource {
+	func numberOfRows(in tableView: NSTableView) -> Int {
+		return tableData.count
+	}
+	
+	func tableView(_ tableView: NSTableView, objectValueFor tableColumn: NSTableColumn?, row: Int) -> Any? {
+		guard let identifier = tableColumn?.identifier else { return nil }
+		guard let col = Int(identifier) else { return nil }
+		guard tableData.indices.contains(row) else { return nil }
+		let rowData = tableData[row]
+		guard rowData.indices.contains(col) else { return nil }
+		return rowData[col]
+	}
+}
+
